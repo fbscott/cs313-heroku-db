b44d320e41a6d039f93c0899acb6f7a4d020e2d9
scott <scottcurrell@gmail.com>
2020-11-21 19:50:57 -0700

    working week 10 milestone

--- server.js
+++ server.js
@@ -13,18 +13,26 @@ const pool             = new Pool({connectionString: connectionString});
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0' // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
 
-var sql = 'SELECT * FROM person';
+// var sql = 'SELECT * FROM person';
+var sql = `SELECT first, last, party, image
+           FROM president AS pres
+           JOIN details AS d
+           ON pres.id = d.president_id
+           JOIN image as i
+           ON i.id = d.image_id
+           JOIN party as p
+           ON p.id = d.party_id`;
 
 pool.query(sql, function(err, result) {
     // If an error occurred...
     if (err) {
-        console.log('Error in query: ')
-        console.log(err);
+        // console.log('Error in query: ')
+        // console.log(err);
     }
 
     // Log this to the console for debugging purposes.
-    console.log('Back from DB with result:');
-    console.log(result.rows);
+    // console.log('Back from DB with result:');
+    // console.log(result.rows);
 });
 
 // allow server to use anything that lives in /public
@@ -38,39 +46,38 @@ APP.get('/', (req, res) => {
     res.sendFile(PATH.join(__dirname + '/public/index.htm'));
 });
 
-
-
-
-
-
-APP.get('/getPerson', function(req, res) {
+APP.get('/getPresident', function(req, res) {
 
     const id = req.query.id;
     // res.render('');
 
-    var sql = 'SELECT * FROM person WHERE id = $1';
+    // var sql = 'SELECT * FROM person WHERE id = $1';
+    var sql = `SELECT first, last, party, image
+               FROM president AS pres
+               JOIN details AS d
+               ON pres.id = d.president_id
+               JOIN image as i
+               ON i.id = d.image_id
+               JOIN party as p
+               ON p.id = d.party_id
+               WHERE pres.id = $1`;
     var values = [id];
 
     pool.query(sql, values, function(err, result) {
         // If an error occurred...
         if (err) {
-            console.log('Error in query: ')
-            console.log(err);
+            // console.log('Error in query: ')
+            // console.log(err);
         }
 
         // Log this to the console for debugging purposes.
-        console.log('Back from DB with result:');
-        console.log(result.rows);
+        // console.log('Back from DB with result:');
+        // console.log(result.rows);
 
         res.json(result.rows);
     });
 
 });
-
-
-
-
-
 
 APP.listen(PORT, () => {
     console.log('Listening on ' + PORT);
