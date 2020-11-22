0fd406d247a98c056d1d46ff3f4977dd5ade193b
scott <scottcurrell@gmail.com>
2020-11-22 14:47:40 -0700

    add partials and fix select to contain pres names

--- server.js
+++ server.js
@@ -14,27 +14,6 @@ const pool             = new Pool({connectionString: connectionString});
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0' // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs
 
-// var sql = 'SELECT * FROM person';
-var sql = `SELECT first, last, party, image
-           FROM president AS pres
-           JOIN details AS d
-           ON pres.id = d.president_id
-           JOIN image as i
-           ON i.id = d.image_id
-           JOIN party as p
-           ON p.id = d.party_id`;
-
-pool.query(sql, function(err, result) {
-    // If an error occurred...
-    if (err) {
-        // console.log('Error in query: ')
-        // console.log(err);
-    }
-
-    // Log this to the console for debugging purposes.
-    // console.log('Back from DB with result:');
-    // console.log(result.rows);
-});
 
 // allow server to use anything that lives in /public
 APP.use(EXPRESS.static(__dirname + '/public'));
@@ -44,33 +23,57 @@ APP.set('views', './views'); // object, directory
 APP.set('view engine', 'ejs'); // render .ejs files as views
 
 APP.get('/', (req, res) => {
-    res.sendFile(PATH.join(__dirname + '/public/index.htm'));
-});
-
-APP.get('/getPresident', function(req, res) {
-
-    const id = req.query.id;
-    // res.render('');
-
-    // var sql = 'SELECT * FROM person WHERE id = $1';
-    var values = [id];
-    var sql = `SELECT first, last, party, image
+    let _title = 'U.S. Presidents';
+    let _intro = 'Select a U.S. president by index (the order in which they served).';
+    var sql = `SELECT first, middle, last, party, image
                FROM president AS pres
                JOIN details AS d
                ON pres.id = d.president_id
                JOIN image as i
                ON i.id = d.image_id
                JOIN party as p
-               ON p.id = d.party_id
-               WHERE pres.id = $1`;
+               ON p.id = d.party_id`;
 
-    pool.query(sql, values, function(err, result) {
+    pool.query(sql, function(err, result) {
+        let pres = new PRESIDENT(result.rows);
+        // If an error occurred...
+        if (err) {
+            console.log('Error in query: ')
+            console.log(err);
+        }
+
+        // Log this to the console for debugging purposes.
+        // console.log('Back from DB with result:');
+        // console.log(result.rows);
+        // res.sendFile(PATH.join(__dirname + '/public/index.htm'));
+        res.render('pages/index', {
+            title: _title,
+            intro: _intro,
+            presidents: result.rows
+        });
+    });
+});
+
+APP.get('/getPresident', function(req, res) {
+    const _id   = req.query.id;
+    const _sql  = `SELECT first, middle, last, party, image
+                   FROM president AS pres
+                   JOIN details AS d
+                   ON pres.id = d.president_id
+                   JOIN image as i
+                   ON i.id = d.image_id
+                   JOIN party as p
+                   ON p.id = d.party_id
+                   WHERE pres.id = $1`;
+    let _values = [_id];
+
+    pool.query(_sql, _values, function(err, result) {
         // var pres = new PRESIDENT(result.rows[0]);
 
         // If an error occurred...
         if (err) {
-            // console.log('Error in query: ')
-            // console.log(err);
+            console.log('Error in query: ')
+            console.log(err);
         }
 
         // Log this to the console for debugging purposes.
@@ -79,7 +82,6 @@ APP.get('/getPresident', function(req, res) {
         res.json(result.rows);
         // res.render('pages/test', pres);
     });
-
 });
 
 APP.listen(PORT, () => {
